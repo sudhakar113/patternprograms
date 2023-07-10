1e4c149ec62cb10cc0a46ad5c4000e92baf76875
sudhakar113 <119921610+sudhakar113@users.noreply.github.com>
2023-07-10 19:28:10 +0530

    Add files via upload

---
+++ Typecast.java
@@ -0,0 +1,39 @@
+package typecating.com;
+
+public class Typecast {
+
+	public static void main(String args[])
+	{
+		/*int m=10,n=10;
+		int res=m++/(++n * n--)/--m;
+		System.out.println(res);
+		
+	}
+}*/
+		/*int x=001,y=010,z=100;
+		int i=--x + y++ - z-- - --z + ++y - --x + y-- - --x;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(i);
+	}
+}*/
+		/*int ch='A';
+		System.out.println(++ch);
+		System.out.println(ch);
+	}
+}*/
+		/*short a=100;
+		short b=45;
+		short c=a+b;
+		System.out.println(c);
+	}
+}*/
+		int a=65;
+		int b=2;
+		float c=a/b;
+		System.out.println(c);
+	}
+}
+
+		
---
+++ Typecasting.java
@@ -0,0 +1,85 @@
+package typecating.com;
+
+public class Typecasting {
+	public static void main(String args[])
+	{
+		//implici type casting
+		/*char a='A';
+		int b;
+		b=a;
+		System.out.println(a);
+		System.out.println(b);
+	
+}
+}*///explicit typecasting
+		/*double a=45.5;
+		byte b;
+		b= (byte) a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}*/
+		/*byte a=1;
+		byte b=1;
+		byte c=(byte)(a+b);
+		System.out.println(a);
+		System.out.println(b);
+	}
+}*/
+		/*int a=25;
+		int b=2;
+		int c=a/b;
+		System.out.println(a);
+		System.out.println(c);
+	}
+}*/
+		/*char a='a';
+		int b=a;
+		b=b+1;
+		char c=(char)b;
+		System.out.println(c);
+	}
+}*/
+		/*double a=131;
+		byte b;
+		b=(byte)a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}*/
+		/* convert byte to int*/
+/*byte a=98;
+//int b;
+//float b;
+//long b;
+//b=a;
+System.out.println(a);
+System.out.println(b);
+	}
+}*/
+		//explict convert
+		/*float a=98.0f;
+		double b;
+		//b=(byte)a;
+		b=a;
+		//float b;
+		//long b;
+		//b=a;
+		System.out.println(a);
+		System.out.println(b);
+			}
+		}*/
+		/*long a=10019;
+		float b;
+		b=a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}*/
+		short b=32767;
+		b++;
+		b++;
+		System.out.println(b);
+	}
+}
+
